ec639d174c660ea457801f5ab406c973313b454b
MyAw <3034993155@qq.com>
2021-06-28 22:03:03 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,7 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <h1>Appp</h1>
+    <router-view />
   </div>
 </template>
 
